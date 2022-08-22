6ba4b7cf2e59b5383551c2460fc692609e90a585
Julius Vinnen <julius@vinnen.net>
2022-08-22 11:45:39 +0200

    Add spheres on veritices for eye gaze collision

--- Assets/Scripts/Debug/LoggerCAD.cs
+++ Assets/Scripts/Debug/LoggerCAD.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using TiltBrush;
 
 public class LoggerCAD : MonoBehaviour
 {
@@ -11,9 +12,18 @@ public class LoggerCAD : MonoBehaviour
     Transform mainCameraTransform;
     Transform wandTransform;
     Transform brushTransform;
+
+    public Batch b;
+    public GeometryPool p;
+
+    public List<Vector3> verts;
+    private int previousCount = 0;
+
+    public GameObject spherePref;
     // Start is called before the first frame update
     void Start()
     {
+        verts = new List<Vector3>();
         filename = "_StudyLog.txt";
         mainCameraTransform = GameObject.Find("Camera (eye)").transform;
         wandTransform = GameObject.Find("Controller (wand)").transform;
@@ -33,6 +43,25 @@ public class LoggerCAD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(b != null)
+        {
+            p = b.Geometry;
+            if (p.m_Vertices.Count != previousCount)
+            {
+                foreach (Vector3 ver in p.m_Vertices)
+                {
+                    if (!verts.Contains(ver))
+                    {
+                        GameObject tmp = Instantiate(spherePref);
+                        tmp.transform.position = ver;
+                        verts.Add(ver);
+                    }
+
+                }
+                previousCount = p.m_Vertices.Count;
+            }
+        }
+
         TextWriter tw = new StreamWriter(filename, true);
 
         tw.WriteLine("[" + System.DateTime.Now + "]" + "Head position: " + mainCameraTransform.position + "Wand position: " + wandTransform.position + "Brush position: " + brushTransform.position);
